aca6b50fa0c100f3139b35b6a0381a0d8663b07d
Rafael Bassini <rafaelbassini@gmail.com>
2019-12-06 19:14:50 -0300

    ajuste

--- src/database/migrations/20191003103953-create-abrigo.js
+++ src/database/migrations/20191003103953-create-abrigo.js
@@ -12,6 +12,11 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
+      slug: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
       email: {
         type: Sequelize.STRING,
         allowNull: false,
